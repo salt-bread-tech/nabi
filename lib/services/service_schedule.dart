b1d3a038210afa64e98db3d8f48d21c5a4382fc5
jiyoon <skymap02@naver.com>
2024-04-11 22:54:38 +0900

    Fix: 오류 수정

--- lib/services/service_schedule.dart
+++ lib/services/service_schedule.dart
@@ -50,7 +50,7 @@ Future<List<Schedule>> fetchSchedules(int userUid, String dateTime) async {
     },
     body: jsonEncode({
       'userUid': userUid,
-      'dateTime': dateTime,
+      'date': dateTime,
     }),
   );
   List<Schedule> schedules = [];
