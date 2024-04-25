7f7a06ee55d92e173407634f881ca62bab80233e
연재다 <revnque@gmail.com>
2024-04-25 20:21:30 +0900

    FEAT: 처방전에 없는 약 등록 api 연결

---
+++ lib/screen/screen_settings.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('설정',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w600)),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SwitchListTile(
+              title: Text('기능1',style: TextStyle(fontSize: 14)),
+              value: true,
+              onChanged: (bool newValue) {
+
+              },
+            ),
+            ListTile(
+              title: Text('신체 정보 수정하기',style: TextStyle(fontSize: 14)),
+              trailing: Icon(Icons.arrow_forward_ios_rounded, size: 15),
+              onTap: () {
+
+              },
+            ),
+            ListTile(
+              title: Text('로그아웃',style: TextStyle(fontSize: 14)),
+              trailing: Icon(Icons.arrow_forward_ios_rounded, size: 15),
+              onTap: () {
+
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
